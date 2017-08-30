2e26ceab7d9856f239033df5aad7635048d1164d
Angelo Cardellicchio <a.cardellicchio@gmail.com>
2017-08-30 10:22:03 +0200

    0.5.1a

---
+++ applications/convnets/siamese_convnet.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import numpy as np
+
+import random
+
+from keras.models import Sequential
+from keras.layers.core import *
+from keras.optimizers import SGD, RMSprop
+from keras import backend as k
+
+https://gist.github.com/mmmikael/0a3d4fae965bdbec1f9d
