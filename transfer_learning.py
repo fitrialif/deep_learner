3c1e3163436bc0f476c23c6c9455c7c6cb9c5cd9
Angelo Cardellicchio <a.cardellicchio@gmail.com>
2017-09-22 14:14:10 +0200

    0.1.0a

---
+++ transfer_learning.py
@@ -0,0 +1,199 @@
+"""Transfer Learning utils for Keras.
+
+Transfer learning and fine tuning follows the implementation defined by Yosinski et al.:
+http://papers.nips.cc/paper/5347-how-transferable-are-features-in-deep-neural-networks.pdf
+
+"""
+
+import os
+import sys
+import glob
+import argparse
+import utils
+
+from keras import applications
+from keras import metrics
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential, Model
+from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
+
+n=1
+
+# Parameters
+img_width, img_height = 75*n, 187*n
+
+# TODO: REFACTORING
+def transfer_learning(base_model, model, model_load, optimizer=optimizers.SGD(lr=0.01, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy']):
+    """
+    Freeze layers from the base model.
+    
+    :param base_model: The part of the model that will not be re-trained for transfer learning.
+    :param model: The 
+    """
+    if model_load:
+        model.load_weights(model_load)
+    for layer in base_model.layers:
+        layer.trainable = False
+    model.compile(
+        optimizer=optimizer,
+        loss=loss,
+        metrics=metrics)
+
+
+def replace_classification_layer(base_model, nb_classes, fc_size):
+    """
+    Replace the classification layer in a pre-trained model. Uses the Keras Functional API.
+    
+    Arguments:
+
+    Return:
+
+    """
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(fc_size, activation='relu')(x)
+    predictions = Dense(nb_classes, activation='softmax')(x)
+    model = Model(input=base_model.input, output=predictions)
+    return model
+
+
+def setup_fine_tuning(model, layers_to_freeze, model_load):
+    """
+    Setup the network for fine tuning.
+    The first layers_to_freeze layers will be made not trainable, while the others will be fine-tuned on the novel dataset.
+    Arguments:
+
+    Return:
+
+    """
+    for layer in model.layers[:layers_to_freeze]:
+        layer.trainable = False
+    for layer in model.layers[layers_to_freeze:]:
+        layer.trainable = True
+    model.compile(optimizer=optimizers.SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+def TransferLearning(args):
+    """
+    Performs training.
+    """
+    train_dir = args.train_folder
+    val_dir = args.validation_folder
+    nb_train_samples = utils.get_nb_files(args.train_folder)
+    nb_val_samples = utils.get_nb_files(args.validation_folder)
+    nb_classes = utils.get_labels(args.train_folder, args.validation_folder)
+    nb_epochs = int(args.nb_epoch)
+    batch_size = int(args.batch_size)
+    base_architecture = args.base_architecture
+    model_load = args.model_load
+    # Define base layer
+    if base_architecture == 'VGG16':
+        base_model = applications.VGG16(weights='imagenet', include_top=False)
+        layers_to_freeze = 10
+    elif base_architecture == 'VGG19':
+        base_model = applications.VGG19(weights='imagenet', include_top=False)
+        layers_to_freeze = 11
+    elif base_architecture == 'InceptionV3':
+        base_model = applications.InceptionV3(weights='imagenet', include_top=False)
+        layers_to_freeze = 172 # TODO: understand how many levels!
+    elif base_architecture == 'ResNet50':
+        base_model = applications.ResNet50(weights='imagenet', include_top=False)
+
+    model = replace_classification_layer(base_model, nb_classes, 1024)
+    
+    # Data augmentation.
+    if not args.train_datagen:
+        train_datagen = ImageDataGenerator(
+            rescale = 1./255,
+            horizontal_flip=True,
+            fill_mode='nearest',
+            width_shift_range=0.05,
+            height_shift_range=0.05)
+    else:
+        train_datagen = args.train_datagen
+    if not args.test_datagen:        
+        test_datagen = ImageDataGenerator(
+            rescale = 1./255,
+            horizontal_flip=True,
+            fill_mode='nearest',
+            width_shift_range=0.05,
+            height_shift_range=0.05)
+    else:
+        test_datagen = args.test_datagen
+
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='categorical')
+    validation_generator = test_datagen.flow_from_directory(
+        val_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='categorical')
+    
+
+    transfer_learning(base_model, model, model_load)
+
+    history_tl = model.fit_generator(
+        train_generator,
+        nb_epoch=nb_epochs,
+        samples_per_epoch=nb_train_samples,
+        validation_data=validation_generator,
+        nb_val_samples=nb_val_samples,
+        class_weight='auto',
+        callbacks=args.callbacks)
+    
+    utils.plot_training(history_tl)
+
+    setup_fine_tuning(model, layers_to_freeze, model_load)
+
+    history_ft = model.fit_generator(
+        train_generator,
+        samples_per_epoch=nb_train_samples,
+        nb_epoch=nb_epochs,
+        validation_data=validation_generator,
+        nb_val_samples=nb_val_samples,
+        class_weight='auto',
+        callbacks=args.callbacks)
+    
+    # NOTE
+    model.save(os.path.join(os.getcwd(), 'models', args.output_model_file))
+
+    utils.plot_training(history_ft)
+
+
+
+if __name__=='__main__':
+    # Callbacks array
+    tensorboard = TensorBoard(log_dir='./logs')
+    early = EarlyStopping(monitor='val_acc', min_delta=0.01, patience=3, verbose=1, mode='auto')
+    checkpoint = ModelCheckpoint("weights.hdf5", monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    callbacks = [tensorboard, early, checkpoint]
+    a = argparse.ArgumentParser()
+    a.add_argument("--base_architecture", default='InceptionV3')
+    a.add_argument("--train_folder", default='./data/train')
+    a.add_argument("--validation_folder", default='./data/test')
+    a.add_argument("--nb_epoch", default=25)
+    a.add_argument("--batch_size", default=128)
+    a.add_argument("--output_model_file", default="inceptionv3.model")
+    a.add_argument("--plot", action="store_true")
+    a.add_argument("--model_load", default='')
+    a.add_argument("--callbacks", default = callbacks)
+    a.add_argument("--train_generator", default=None)
+    a.add_argument("--validation_generator", default=None)
+
+    args = a.parse_args()
+
+    if args.train_folder is None or args.validation_folder is None:
+        a.print_help()
+        sys.exit(1)
+
+    if (not os.path.exists(args.train_folder)) or (not os.path.exists(args.validation_folder)):
+        print("directories do not exist")
+        sys.exit(1)
+
+    TransferLearning(args)
---
+++ utils.py
@@ -0,0 +1,155 @@
+import os, glob, sys
+import numpy as np
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+#from keras import backend as K
+from sklearn.model_selection import train_test_split
+from keras import optimizers
+from pandas import DataFrame
+from pandas import concat
+
+def set_image_format(img_rows, img_cols, img_channels, keras_backend):
+    """
+    """
+    if keras_backend == 'channels_first':
+        input_shape = (img_channels, img_rows, img_cols)
+    else:
+        input_shape = (img_rows, img_cols, img_channels)
+
+
+def get_nb_files(root_folder):
+    """
+    Get the number of files in a folder's hierarchy.
+    Arguments:
+        root_folder: A string which represents the root folder.
+    Returns:
+        file_count: An integer which represents the number of files in the folder's hierarchy.
+    """
+    if not os.path.exists(root_folder):
+        sys.exit('Root folder does not exist.')
+    file_count = 0
+    for r, dirs, files in os.walk(root_folder):
+        for dr in dirs:
+            file_count += len(glob.glob(os.path.join(r, dr + "/*")))
+    return file_count
+
+
+def get_labels(train_folder, validation_folder):
+    """Get the number of labels.
+    Arguments:
+        train_folder: A string which represents the relative path of the train folder.
+        validation_folder: A string which represents the relative path of the validation folder.
+    Return:
+        num_labels: An integer which represents the number of labels for the images on which the network will be trained.
+    """
+    if not os.path.exists(train_folder):
+        sys.exit('Train folder does not exist.')
+    if not os.path.exists(validation_folder):
+        sys.exit('Validation folder does not exist.')
+    if not (len(glob.glob(train_folder + "/*")) == len(glob.glob(validation_folder + "/*"))):
+        sys.exit('The number of training labels is different from the number of validation labels.')
+    else:
+        num_labels = len(glob.glob(train_folder + "/*"))
+        return num_labels
+
+
+
+
+
+def makedirs_wrapper(folder):
+    """Simple wrapper around the os.makedirs function. It automatically checks if the argument folder exists, and creates it if not.
+    """
+    if not os.path.exists(folder):
+        try:
+            os.makedirs(folder)
+        except OSError:
+            pass
+
+
+def split_each_label(root_folder, min_examples=0):
+    """
+    Algorithm:
+    1. Check if root_folder exists
+    2. Create train_folder and test_folder
+    3. TODO: CREATE THE SUBFOLDER STRUCTURE FROM STRING NAME
+    4. For each subfolder in the subfolder structure, check length and create sub-train and sub-test folders
+    5. Split training and testing set
+    6. Move images into proper folders
+    7. TODO: remove unused folders
+    """
+    if not os.path.exists(root_folder):
+        sys.exit("Root folder does not exist.")
+    # Check if train and test folder exist. If not, create them.
+    train_folder = os.path.abspath(os.path.join(root_folder, 'train'))
+    test_folder = os.path.abspath(os.path.join(root_folder, 'test'))
+    makedirs_wrapper(train_folder)
+    makedirs_wrapper(test_folder)
+    for root, dirs, files in os.walk(root_folder):
+        for d in dirs:
+            act_len = len(glob.glob(os.path.join(root, d + "/*")))
+            dir_path = os.path.join(root_folder, d)
+            # Ugly hack
+            if d == 'train' or d == 'test':
+                break
+            else:
+                d_train_folder = os.path.join(train_folder, d)
+                d_test_folder = os.path.join(test_folder, d)
+                makedirs_wrapper(d_train_folder)
+                makedirs_wrapper(d_test_folder)
+            if act_len > min_examples:
+                x = y = os.listdir(dir_path)
+                x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+                if x_test:
+                    for x in x_train:
+                        os.rename(os.path.join(dir_path, x), os.path.join(d_train_folder, x))
+                    for x in x_test:
+                        os.rename(os.path.join(dir_path, x), os.path.join(d_test_folder, x))
+
+
+
+
+# Courtesy of Jason Brownlee
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	"""
+	Frame a time series as a supervised learning dataset.
+	Arguments:
+		data: Sequence of observations as a list or NumPy array.
+		n_in: Number of lag observations as input (X).
+		n_out: Number of observations as output (y).
+		dropnan: Boolean whether or not to drop rows with NaN values.
+	Returns:
+		Pandas DataFrame of series framed for supervised learning.
+	"""
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+
+def plot_training(history):
+    val_acc = history.history['val_acc']
+    val_loss = history.history['val_loss']
+    epochs = range(len(history.history['acc']))
+    plt.plot(epochs, val_acc, 'r')
+    plt.title('Validation accuracy')
+    plt.figure()
+    plt.plot(epochs, val_loss, 'r-')
+    plt.title('Validation loss')
+    plt.show()
